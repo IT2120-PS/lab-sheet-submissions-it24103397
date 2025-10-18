bb083eeea1592d7932857e8d80a14e48fc2980db
it24103397 <it24103397@my.sliit.lk>
2025-10-18 15:18:13 +0530

    Add files via upload

---
+++ Lab Sheet 10/IT24103397Lab sheet10.R
@@ -0,0 +1,17 @@
+setwd("C:\\Users\\sana\\Desktop\\IT24103397")
+
+#Exercise
+#Q1(i)
+observed <- c(120,95,85,100)
+prob <- c(0.25,0.25,0.25,0.25)
+
+#(ii)
+chisq.test(x=observed, p=prob)
+
+#(iii)
+#Conclution: Since the p value(0.08966) is greater than 0.05,do not  reject null hypothesis at 5%
+#level of significance. therefore we can conclude that there is a significant association between
+#snack and type
+
+
+
